921c4fd568dce448202b8e2c1d231b6e01387282
enspour <enspour@gmail.com>
2025-01-13 15:50:38 +0300

    feat: updated explorer

--- libs/mfr/core/feature-explorer/src/lib/explorer-container.component.ts
+++ libs/mfr/core/feature-explorer/src/lib/explorer-container.component.ts
@@ -1,12 +1,19 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { KuduSidebarComponent } from '@kudu-ui';
-
 import { NgComponentOutlet } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  linkedSignal,
+} from '@angular/core';
+
+import { KuduIconComponent, KuduSidebarComponent } from '@kudu-ui';
+
+import { ExplorerConfig } from './explorer.interface';
 import { ExplorerService } from './explorer.service';
 
 @Component({
   selector: 'lib-explorer-container',
-  imports: [NgComponentOutlet, KuduSidebarComponent],
+  imports: [NgComponentOutlet, KuduSidebarComponent, KuduIconComponent],
   templateUrl: './explorer-container.component.html',
   styleUrl: './explorer-container.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,5 +22,15 @@ export class ExplorerContainerComponent {
   private explorerService = inject(ExplorerService);
 
   public isOpen = this.explorerService.isOpen;
-  public config = this.explorerService.config;
+  public config = linkedSignal<
+    ExplorerConfig<any> | null,
+    ExplorerConfig<any> | null
+  >({
+    source: this.explorerService.config,
+    computation: (config, previous) => config || previous?.value || null,
+  });
+
+  public onClose() {
+    this.explorerService.close();
+  }
 }
